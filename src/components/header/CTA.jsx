58decfaacaaeb28c39684ee5272460d62c8353b8
olaoyetijani <adeolatijani12@gmail.com>
2023-06-07 07:08:54 +0100

    profile picture

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './header.css'
-import CV from '../../assets/tijani_cv.pdf'
+import cv from '../../assets/tijani_cv.pdf'
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className='btn'>Download CV</a>
+      <a href={cv} download className='btn'>Download CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
