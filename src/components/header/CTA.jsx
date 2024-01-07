486104ea3446f22d41a354165105edc6a8d208b2
Hussein Sheu Tijani <adeolatijani12@gmail.com>
2024-01-07 09:40:54 +0100

    new project

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import './header.css'
-import cv from '../../assets/tijani_cv.pdf'
+import React from "react";
+import "./header.css";
+import cv from "../../assets/Tijani__CV.pdf";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={cv} download className='btn'>Download CV</a>
-      <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+      <a href={cv} download className="btn">
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Let's Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
